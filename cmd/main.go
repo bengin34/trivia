4c32ceacfb742c3bac647579319bb229f57149dc
bengin34 <becaglar3434@gmail.com>
2023-03-28 21:18:09 +0300

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,41 @@
+// package main
+
+// import (
+//     "github.com/bengin34/trivia/database"
+//     "github.com/gofiber/fiber/v2"
+//     "github.com/gofiber/fiber/v2/middleware/cors"
+// )
+
+// func main() {
+//     database.ConnectDb()
+//     app := fiber.New()
+
+//     setupRoutes(app)
+
+   
+
+//     app.Listen(":3000")
+// }
+
 package main
 
 import (
-    "github.com/gofiber/fiber/v2"
     "github.com/bengin34/trivia/database"
+    "github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
     database.ConnectDb()
     app := fiber.New()
 
-    setupRoutes(app)
+    // Enable CORS with custom configuration
+    app.Use(cors.New(cors.Config{
+        AllowOrigins: "http://localhost:3001",
+        AllowHeaders: "Origin, Content-Type, Accept",
+    }))
 
-   
+    setupRoutes(app)
 
     app.Listen(":3000")
 }
