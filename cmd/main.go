021a8ae4c0a55a0db5d07ae50a0b4f4995792304
bengin34 <becaglar3434@gmail.com>
2023-03-28 16:29:14 +0300

    first api

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "github.com/gofiber/fiber/v2"
+    "github.com/bengin34/trivia/database"
+)
+
+func main() {
+    database.ConnectDb()
+    app := fiber.New()
+
+    setupRoutes(app)
+
+   
+
+    app.Listen(":3000")
+}
